1af0f71263489312b534c64a7433890ea7771000
foudfou <foudil.newbie+git@gmail.com>
2011-05-22 15:07:23 +0200

    failed attempt with TabsProgressListener (but better understanding of the code)

--- src/chrome/content/overlay.js
+++ src/chrome/content/overlay.js
@@ -1,7 +1,147 @@
 /* -*- Mode: javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
+/* ***** BEGIN LICENSE BLOCK *****
+*   Version: MPL 1.1/GPL 2.0/LGPL 2.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Original Code is MitM Me.
+*
+* The Initial Developer of the Original Code is
+* Johnathan Nightingale.
+* Portions created by the Initial Developer are Copyright (C) 2008
+* the Initial Developer. All Rights Reserved.
+*
+* Contributor(s):
+* Andras TIM - the new developer @ 2010
+* andras.tim@gmail.com, andras.tim@balabit.hu
+* Foudil Brétel <foudil.newbie@bigfoot.com>
+*
+* Alternatively, the contents of this file may be used under the terms of
+* either the GNU General Public License Version 2 or later (the "GPL"), or
+* the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+* in which case the provisions of the GPL or the LGPL are applicable instead
+* of those above. If you wish to allow use of your version of this file only
+* under the terms of either the GPL or the LGPL, and not to allow others to
+* use your version of this file under the terms of the MPL, indicate your
+* decision by deleting the provisions above and replace them with the notice
+* and other provisions required by the GPL or the LGPL. If you do not delete
+* the provisions above, a recipient may use your version of this file under
+* the terms of any one of the MPL, the GPL or the LGPL.
+*
+* ***** END LICENSE BLOCK ***** */
+
 var mitm_me = {
   DEBUG_MODE: true,
+
+  TabsProgressListener: {
+
+    onSecurityChange: function (aWebProgress, aRequest, aState) {
+      mitm_me.dump("onSecurityChange");
+      var uri = gBrowser.currentURI;
+
+      if (uri.schemeIs("https")) {
+
+        mitm_me.dumpObj(this);
+
+        // get SSLStatus from nsISSLStatusProvider
+        gSSLStatus = gBrowser.securityUI
+          .QueryInterface(Components.interfaces.nsISSLStatusProvider)
+          .SSLStatus;
+
+        // otherwise get SSLStatus from recentBadCertsService
+        if(!gSSLStatus) {
+          mitm_me.dump("onSecurityChange: no SSLStatus from nsISSLStatusProvider, trying recentBadCertsService");
+          try {
+            if (!mitm_me._recentBadCertsService) {
+              Components.utils.reportError("MITME: no recentBadCertService ?!");
+              return;
+            }
+            var hostWithPort = uri.host + ":" + uri.port;
+            mitm_me.dump("onSecurityChange: "+hostWithPort+" gSSLStatus="+gSSLStatus);
+            gSSLStatus = mitm_me._recentBadCertsService.getRecentBadCert(hostWithPort);
+            if(gSSLStatus)
+              mitm_me.dump("gSSLStatus defined !")
+            else
+              mitm_me.dump("gSSLStatus undefined !")
+          }
+          catch (e) {
+            Components.utils.reportError(e);
+            return;
+          }
+        }
+
+        // Get the cert (XHR)
+        mitm_me.getCert(uri);
+				mitm_me.dumpObj(gSSLStatus);
+
+        // Ultimate check for SSLStatus
+        if(!gSSLStatus) {
+          Components.utils.reportError("MITMME - No gSSLStatus on attempt to add exception")
+          return;
+        }
+        if(!gCert){
+          Components.utils.reportError("MITMME - No gCert on attempt to add exception")
+          return;
+        }
+
+        // Add the exception
+        // TODO: useful ? right place ?
+        // if (!mitm_me._overrideService) {
+        //   Components.utils.reportError("MITME: no overrideService ?!");
+        //   return;
+        // }
+        var flags = 0;
+        if(gSSLStatus.isUntrusted)
+          flags |= mitm_me._overrideService.ERROR_UNTRUSTED;
+        if(gSSLStatus.isDomainMismatch)
+          flags |= mitm_me._overrideService.ERROR_MISMATCH;
+        if(gSSLStatus.isNotValidAtThisTime)
+          flags |= mitm_me._overrideService.ERROR_TIME;
+
+        mitm_me._overrideService.rememberValidityOverride(
+          uri.asciiHost, uri.port,
+          gCert,
+          flags,
+          mitm_me._prefService.getBoolPref("add_temporary_exceptions"));
+
+        // // Reload the page
+        // gBrowser.reloadTab(gBrowser.mCurrentTab);
+        // mitm_me.dump("page reloaded");
+        // mitm_me.dumpObj(aWebProgress);
+        aWebProgress.reload();
+
+      }
+    },
+
+    onStateChange: function (aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
+      // Attach a listener to watch for "click" events bubbling up from error
+      // pages and other similar page.
+      // We can't look for this during onLocationChange since at that point the
+      // document URI is not yet the about:-uri of the error page.
+
+      if (aStateFlags & Ci.nsIWebProgressListener.STATE_STOP &&
+          /^about:certerror/.test(aWebProgress.DOMWindow.document.documentURI)) {
+        mitm_me.dump("onStateChange: certerror !");
+
+        aBrowser.addEventListener("click", mitm_me.onClick, false);
+        aBrowser.addEventListener("pagehide", function () {
+          aBrowser.removeEventListener("click", mitm_me.onClick, false);
+          aBrowser.removeEventListener("pagehide", arguments.callee, true);
+        }, true);
+
+      }
+    },
+
+  },
 
   onLoad: function() {
     // initialization code
@@ -15,29 +155,33 @@ var mitm_me = {
     this._prefService.QueryInterface(Components.interfaces.nsIPrefBranch2);
     this._prefService.addObserver("", this, false);
 
+    // Get the cert
+    this._recentBadCertsService =
+      Cc["@mozilla.org/security/recentbadcerts;1"]
+      .getService(Components.interfaces.nsIRecentBadCertsService);
+
+    this._overrideService =
+      Cc["@mozilla.org/security/certoverride;1"]
+      .getService(Components.interfaces.nsICertOverrideService);
+
   //   window.setTimeout(this.delayedStartup, 1000); // 1s
   // },
   // delayedStartup: function() {
 
-    // Add click handler in place of browser's
-    // if (typeof BrowserOnCommand != "undefined")
-    //   gBrowser.removeEventListener("command", BrowserOnCommand, false);
-    gBrowser.removeEventListener("click", BrowserOnClick, false);
-    // TODO: harder to replace BrowserOnClick which is attached through a TabsProgressListener...
-    gBrowser.addEventListener("click", this.onClick, false);
+    try {
+      // gBrowser.removeTabsProgressListener(window.TabsProgressListener);
+      gBrowser.addTabsProgressListener(mitm_me.TabsProgressListener);
+    } catch (ex) {
+      Components.utils.reportError(ex);
+    }
 
-    gBrowser.addEventListener("command", this.onCommand, false);
-    var silent = this._prefService.getBoolPref("silent_mode");
-    this.dump('silent_mode: '+silent);
-    if (silent)
-      document.getElementById("content")
-      .addEventListener("DOMLinkAdded", this.onCommand, false);
+    this.dump('MITME LOADED !');
   },
 
+  // TODO: this is where we're going to handle the 'silent' option
   onClick: function(event) {
-    Components.utils.reportError(event);
+    // Components.utils.reportError(event);
     mitm_me.dump("onClick");
-    mitm_me.dumpObj(event);
   },
 
   onCommand: function(event) {
@@ -48,6 +192,9 @@ var mitm_me = {
     var ot = event.originalTarget;
     var errorDoc = ot.ownerDocument;
     var uri = gBrowser.currentURI;
+
+    mitm_me.dump("originalTarget:");
+    // mitm_me.dumpObj(ot.ownerDocument);
 
     // If the event came from an ssl error page
     // optional semi-automatic "Add Exception" button event...
@@ -147,11 +294,57 @@ var mitm_me = {
       gChecking = false;
     }
 
+    if (req.channel)
+      mitm_me.dump("req.channel defined");
+    else
+      mitm_me.dump("req.channel undef");
+
+    // WHERE GETS gSSLStatus CREATED ?!
+
     if(req.channel && req.channel.securityInfo) {
-      const Ci = Components.interfaces;
-      gSSLStatus = req.channel.securityInfo
-                      .QueryInterface(Ci.nsISSLStatusProvider).SSLStatus;
+      var secInfo = req.channel.securityInfo;
+
+			mitm_me.dump("req.channel.securityInfo OK");
+
+      gSSLStatus = secInfo.QueryInterface(Ci.nsISSLStatusProvider).SSLStatus;
       gCert = gSSLStatus.QueryInterface(Ci.nsISSLStatus).serverCert;
+
+		  if (secInfo instanceof Ci.nsISSLStatusProvider) {
+
+			  var cert = secInfo.QueryInterface(Ci.nsISSLStatusProvider).
+			    SSLStatus.QueryInterface(Ci.nsISSLStatus).serverCert;
+			  var verificationResult = cert.verifyForUsage(Ci.nsIX509Cert.CERT_USAGE_SSLServer);
+
+			  switch (verificationResult) {
+				case Ci.nsIX509Cert.VERIFIED_OK:
+					mitm_me.dump("OK");
+					break;
+				case Ci.nsIX509Cert.NOT_VERIFIED_UNKNOWN:
+					mitm_me.dump("not verfied/unknown");
+					break;
+				case Ci.nsIX509Cert.CERT_REVOKED:
+					mitm_me.dump("revoked");
+					break;
+				case Ci.nsIX509Cert.CERT_EXPIRED:
+					mitm_me.dump("expired");
+					break;
+				case Ci.nsIX509Cert.CERT_NOT_TRUSTED:
+					mitm_me.dump("not trusted");
+					break;
+				case Ci.nsIX509Cert.ISSUER_NOT_TRUSTED:
+					mitm_me.dump("issuer not trusted");
+					break;
+				case Ci.nsIX509Cert.ISSUER_UNKNOWN:
+					mitm_me.dump("issuer unknown");
+					break;
+				case Ci.nsIX509Cert.INVALID_CA:
+					mitm_me.dump("invalid CA");
+					break;
+				default:
+					mitm_me.dump("unexpected failure");
+					break;
+			  }
+			}
     }
   },
 
